b6ef9584fda81e4a17d2fc06eab351fa89656c97
ATeaGod <71936369+ATeaGod@users.noreply.github.com>
2025-08-25 17:44:31 +0100

    Added The choice between just letting it show graphs

--- Elipse.py
+++ Elipse.py
@@ -5,6 +5,15 @@ from astropy.io import fits
 from astropy.utils.data import download_file
 from photutils.isophote import Ellipse, build_ellipse_model
 
+# Display Graphs During Creation #
+
+DispGraph = True
+ginput = input("Would You Like To Display Graphs (y/n) ")
+if ginput == "y":
+    DispGraph = True
+if ginput == "n":
+    DispGraph = False
+
 # file stuffs #
 
 filepath = input("FilePath >>> ")
@@ -19,14 +28,42 @@ isolist = ellipse.fit_image(sma0=20.)
 print(isolist.to_table())
 
 # Brightness Graph #
+if DispGraph == True:
+    plt.figure(figsize=(8, 4))
+    plt.scatter(isolist.sma**0.25, -2.5*np.log10(isolist.intens))
+    plt.title("brightness profile")
+    plt.xlabel('sma**1/4')
+    plt.ylabel('Brightness')
+    plt.gca().invert_yaxis()
+    plt.show()
 
-plt.figure(figsize=(8, 4))
-plt.scatter(isolist.sma**0.25, -2.5*np.log10(isolist.intens))
-plt.title("brightness profile")
-plt.xlabel('sma**1/4')
-plt.ylabel('Brightness')
-plt.gca().invert_yaxis()
-plt.show()
+# Something #
+if DispGraph == True:
+    plt.figure(figsize=(10, 5))
+    plt.figure(1)
+
+    plt.subplot(221)
+    plt.errorbar(isolist.sma, isolist.eps, yerr=isolist.ellip_err, fmt='o', markersize=4)
+    plt.xlabel('Semimajor axis length')
+    plt.ylabel('Ellipticity')
+
+    plt.subplot(222)
+    plt.errorbar(isolist.sma, isolist.pa/np.pi*180., yerr=isolist.pa_err/np.pi* 80., fmt='o', markersize=4)
+    plt.xlabel('Semimajor axis length')
+    plt.ylabel('PA (deg)')
+
+    plt.subplot(223)
+    plt.errorbar(isolist.sma, isolist.x0, yerr=isolist.x0_err, fmt='o', markersize=4)
+    plt.xlabel('Semimajor axis length')
+    plt.ylabel('X0')
+
+    plt.subplot(224)
+    plt.errorbar(isolist.sma, isolist.y0, yerr=isolist.y0_err, fmt='o', markersize=4)
+    plt.xlabel('Semimajor axis length')
+    plt.ylabel('Y0')
+
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.35, wspace=0.35)
+    plt.show()
 
 # Model Generation #
 
